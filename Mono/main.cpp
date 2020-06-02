b2871e18298ec1d59bd5c11fb70a896c0586f275
BCOM\nduong <nam-duong.duong@b-com.com>
2020-06-02 14:54:19 +0200

    Update initialization for mono SLAM

--- Mono/main.cpp
+++ Mono/main.cpp
@@ -65,7 +65,7 @@
 #define MIN_THRESHOLD -1
 #define MAX_THRESHOLD 220
 #define NB_THRESHOLD 3
-#define NB_POINTCLOUD_INIT 150
+#define NB_POINTCLOUD_INIT 50
 #define MIN_WEIGHT_NEIGHBOR_KEYFRAME 50
 #define MIN_POINT_DISTANCE 0.04
 
@@ -156,14 +156,13 @@ int main(int argc, char **argv) {
 	std::vector<DescriptorMatch>                        matches;
 	Transform3Df                                        poseFrame;
 	Transform3Df                                        newFramePose;
-	Transform3Df                                        lastPose;
-	std::vector<SRef<CloudPoint>>						cloud, filteredCloud;
+	Transform3Df                                        lastPose;	
 	std::vector<Transform3Df>                           keyframePoses;
 	std::vector<Transform3Df>                           framePoses;
 	SRef<Frame>                                         newFrame;
 	SRef<Frame>											frameToTrack;
 	SRef<Keyframe>                                      referenceKeyframe, updatedRefKf;
-	SRef<Image>                                         imageMatches, imageMatches2;
+	SRef<Image>                                         imageMatches;
 	std::vector<SRef<CloudPoint>>						localMap;
 
 	bool												isLostTrack = false;
@@ -202,6 +201,7 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
+	// camera pose estimation function based on the fiducial marker
 	auto detectFiducialMarker = [&imageConvertor, &imageFilterBinary, &contoursExtractor, &contoursFilter, &perspectiveController,
 		&patternDescriptorExtractor, &patternMatcher, &markerPatternDescriptor, &patternReIndexer, &img2worldMapper, &pnp, &overlay3D](SRef<Image>& image, Transform3Df &pose){
 		SRef<Image>                     greyImage, binaryImage;
@@ -254,9 +254,21 @@ int main(int argc, char **argv) {
 		return marker_found;
 	};
 
+	// calculate distance between two center of camera poses
+	auto centerCamDistance = [](const Transform3Df & pose1, const Transform3Df & pose2) {
+		return std::sqrt(std::pow(pose1(0, 3) - pose2(0, 3), 2.f) + std::pow(pose1(1, 3) - pose2(1, 3), 2.f) +
+			std::pow(pose1(2, 3) - pose2(2, 3), 2.f));
+	};
+
+	// calculate angle between z-axis of two camera poses
+	auto angleCamDistance = [](const Transform3Df & pose1, const Transform3Df & pose2) {
+		return std::acos(pose1(0, 2) * pose2(0, 2) + pose1(1, 2) * pose2(1, 2) + pose1(2, 2) * pose2(2, 2));
+	};
+
 	// Initialization by get two keyframes using pose estimation from fiducial marker
-	SRef<Frame>			frame1, frame2;
-	SRef<Keyframe>      keyframe1, keyframe2;
+	SRef<Frame>						frame1, frame2;
+	SRef<Keyframe>					keyframe1, keyframe2;
+	std::vector<SRef<CloudPoint>>	cloud, filteredCloud;
 	bool bootstrapOk = false;
 	bool initFrame1 = false;
 	while (!bootstrapOk)
@@ -276,18 +288,23 @@ int main(int argc, char **argv) {
 			frame1 = xpcf::utils::make_shared<Frame>(keypoints, descriptors, view, poseFrame);
 		}
 		else {
+			// check center camera distance
 			Transform3Df poseFrame1 = frame1->getPose();
-			float disTwoKeyframes = std::sqrt(std::pow(poseFrame1(0, 3) - poseFrame(0, 3), 2.f) + std::pow(poseFrame1(1, 3) - poseFrame(1, 3), 2.f) +
-				std::pow(poseFrame1(2, 3) - poseFrame(2, 3), 2.f));
-			if (disTwoKeyframes < 0.1) {
+			if (centerCamDistance(poseFrame, poseFrame1) < 0.1) {
                 overlay3D->draw(poseFrame, view);
 				if (imageViewer->display(view) == SolAR::FrameworkReturnCode::_STOP)
 					return 1;
 				continue;
 			}
+			// feature extraction
 			keypointsDetector->detect(view, keypoints);
 			descriptorExtractor->extract(view, keypoints, descriptors);
 			frame2 = xpcf::utils::make_shared<Frame>(keypoints, descriptors, view, poseFrame);
+			// check angle camera distance
+			if (angleCamDistance(poseFrame, poseFrame1) > 0.1) {
+				frame1 = frame2;
+				continue;
+			}
 			// matching
 			matcher->match(frame1->getDescriptors(), frame2->getDescriptors(), matches);
 			matchesFilter->filter(matches, matches, frame1->getKeypoints() , frame2->getKeypoints());
@@ -300,7 +317,7 @@ int main(int argc, char **argv) {
 			triangulator->triangulate(frame1->getKeypoints(), frame2->getKeypoints(), frame1->getDescriptors(), frame2->getDescriptors(), matches,
 				std::make_pair(0, 1), frame1->getPose(), frame2->getPose(), cloud);
 			mapFilter->filter(frame1->getPose(), frame2->getPose(), cloud, filteredCloud);
-
+			std::cout << filteredCloud.size() << std::endl;
             if (filteredCloud.size() > NB_POINTCLOUD_INIT) {
 				// add keyframes to keyframes manager
 				keyframe1 = xpcf::utils::make_shared<Keyframe>(frame1);				
@@ -331,6 +348,7 @@ int main(int argc, char **argv) {
 	LOG_INFO("Number of initial point cloud: {}", pointCloudManager->getNbPoints());
 	LOG_INFO("Number of initial keyframes: {}", keyframesManager->getNbKeyframes());
 
+	// update data function
 	auto updateData = [&](const SRef<Keyframe> &refKf, std::vector<SRef<CloudPoint>> &localMap, SRef<Keyframe> & referenceKeyframe, SRef<Frame> &frameToTrack)
 	{
 		referenceKeyframe = refKf;
@@ -340,7 +358,37 @@ int main(int argc, char **argv) {
 		localMap.clear();
 		// get local point cloud
 		mapper->getLocalPointCloud(refKf, MIN_WEIGHT_NEIGHBOR_KEYFRAME, localMap);
-	};			
+	};	
+
+	// checkDisparityDistance
+	std::function<bool(const SRef<Frame>&)> checkDisparityDistance = [&referenceKeyframe, &mapper, &projector, &pointCloudManager](const SRef<Frame>& newFrame) -> bool {
+		const std::vector<Keypoint> &refKeypoints = referenceKeyframe->getKeypoints();
+		const std::map<uint32_t, uint32_t> &refMapVisibility = referenceKeyframe->getVisibility();
+		std::vector<SRef<CloudPoint>> cpRef;
+		std::vector<Point2Df> projected2DPts, ref2DPts;
+
+		for (auto const & it : refMapVisibility) {
+			SRef<CloudPoint> cloudPoint;
+			if (pointCloudManager->getPoint(it.second, cloudPoint) == FrameworkReturnCode::_SUCCESS) {
+				cpRef.push_back(cloudPoint);
+				ref2DPts.push_back(Point2Df(refKeypoints[it.first].getX(), refKeypoints[it.first].getY()));
+			}
+		}
+		projector->project(cpRef, projected2DPts, newFrame->getPose());
+
+		unsigned int imageWidth = newFrame->getView()->getWidth();
+		double totalMatchesDist = 0.0;
+		for (int i = 0; i < projected2DPts.size(); i++)
+		{
+			Point2Df pt1 = ref2DPts[i];
+			Point2Df pt2 = projected2DPts[i];
+			totalMatchesDist += (pt1 - pt2).norm() / imageWidth;
+		}
+		double meanMatchesDist = totalMatchesDist / projected2DPts.size();
+		LOG_DEBUG("Keyframe Selector Mean Matches Dist: {}", meanMatchesDist);
+		return (meanMatchesDist > 0.07);
+	};
+
 
 	// check need to make a new keyframe based on all existed keyframes
 	std::function<bool(const SRef<Frame>&)> checkNeedNewKfWithAllKfs = [&referenceKeyframe, &mapper, &keyframeRetriever, &updatedRefKf, &keyframesManager](const SRef<Frame>& newFrame) -> bool {
@@ -356,35 +404,6 @@ int main(int argc, char **argv) {
 		}
 		else
 			return false;
-	};
-
-	// checkDisparityDistance
-	std::function<bool(const SRef<Frame>&)> checkDisparityDistance = [&referenceKeyframe, &mapper, &projector, &pointCloudManager](const SRef<Frame>& newFrame) -> bool {
-		const std::vector<Keypoint> &refKeypoints = referenceKeyframe->getKeypoints();
-		const std::map<uint32_t, uint32_t> &refMapVisibility = referenceKeyframe->getVisibility();
-		std::vector<SRef<CloudPoint>> cpRef;
-		std::vector<Point2Df> projected2DPts, ref2DPts;
-
-		for (auto const & it: refMapVisibility) {
-			SRef<CloudPoint> cloudPoint;
-			if (pointCloudManager->getPoint(it.second, cloudPoint) == FrameworkReturnCode::_SUCCESS) {
-				cpRef.push_back(cloudPoint);
-				ref2DPts.push_back(Point2Df(refKeypoints[it.first].getX(), refKeypoints[it.first].getY()));
-			}
-		}
-		projector->project(cpRef, projected2DPts, newFrame->getPose());
-
-		unsigned int imageWidth = newFrame->getView()->getWidth();
-		double totalMatchesDist = 0.0;
-		for (int i = 0; i < projected2DPts.size(); i++)
-		{
-			Point2Df pt1 = ref2DPts[i];
-			Point2Df pt2 = projected2DPts[i];
-            totalMatchesDist += (pt1 - pt2).norm() / imageWidth;
-		}
-		double meanMatchesDist = totalMatchesDist / projected2DPts.size();
-		LOG_DEBUG("Keyframe Selector Mean Matches Dist: {}", meanMatchesDist);
-		return (meanMatchesDist > 0.07);
 	};
 
 	// Update keypoint visibility, descriptor in cloud point
@@ -422,7 +441,7 @@ int main(int argc, char **argv) {
 			if (newKf_mapVisibility.find(i) != newKf_mapVisibility.end()) {
 				checkMatches[i] = true;
 			}
-			
+
 		// Triangulate to neighboring keyframes
 		for (int i = 0; i < idxBestNeighborKfs.size(); ++i) {
 			// get non map point view keypoints
@@ -437,9 +456,7 @@ int main(int argc, char **argv) {
 			const Transform3Df &tmpKf_pose = tmpKf->getPose();
 
 			// check distance between two keyframes
-            float distPose = std::sqrt(std::pow(newKf_pose(0, 3) - tmpKf_pose(0, 3), 2.f) + std::pow(newKf_pose(1, 3) - tmpKf_pose(1, 3), 2.f) +
-                std::pow(newKf_pose(2, 3) - tmpKf_pose(2, 3), 2.f));
-			if (distPose < 0.05)
+			if ((centerCamDistance(newKf_pose, tmpKf_pose) < 0.05) || (angleCamDistance(newKf_pose, tmpKf_pose) > 0.5))
 				continue;
 
 			// Matching based on BoW
@@ -659,8 +676,7 @@ int main(int argc, char **argv) {
 			lastPose = newFrame->getPose();
 
 			// check need new keyframe
-			if (keyframeSelector->select(newFrame, checkDisparityDistance))
-			{
+			if (keyframeSelector->select(newFrame, checkDisparityDistance)){
 				if (keyframeSelector->select(newFrame, checkNeedNewKfWithAllKfs)) {
 					updateData(updatedRefKf, localMap, referenceKeyframe, frameToTrack);
 				}
@@ -724,15 +740,11 @@ int main(int argc, char **argv) {
 		count++;
 	}
 
-	//covisibilityGraph->display();	
-
 	// display stats on frame rate
 	end = clock();
 	double duration = double(end - start) / CLOCKS_PER_SEC;
 	printf("\n\nElasped time is %.2lf seconds.\n", duration);
 	printf("Number of processed frame per second : %8.2f\n", count / duration);
-
-	mapper->saveToFile("Map.xml");
 
 	return 0;
 }
