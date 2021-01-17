cb36244d16bd2e245ee04497b4b4a1c71e3cfe42
BCOM\jroyan <jerome.royan@b-com.com>
2021-01-17 22:49:12 +0100

    Add script to install data and external modules for samples (Windows)

---
+++ sampleInstall.bat
@@ -0,0 +1,13 @@
+@echo off
+:: Download bag of words vocabulary
+curl https://github.com/SolarFramework/SolARModuleFBOW/releases/download/fbowVocabulary/fbow_voc.zip -L -o fbow_voc.zip
+powershell Expand-Archive fbow_voc.zip -DestinationPath .\data\fbow_voc -F
+del fbow_voc.zip
+
+:: Download  captures
+curl https://vision.in.tum.de/rgbd/dataset/freiburg3/rgbd_dataset_freiburg3_long_office_household_validation-rgb.avi -L -o .\data\rgbd_dataset_freiburg3_long_office_household_validation-rgb.avi
+
+
+:: Install required external modules
+remaken install packagedependencies_modules.txt
+remaken install packagedependencies_modules.txt -c debug
